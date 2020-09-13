38a4758196bbf80e675d2a1b849df854369ad30a
shun-tateyama <b1522783@planet.kanazawa-it.ac.jp>
2020-09-13 03:27:41 +0000

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-    root 'pages#home'
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
+
+  root 'posts#index'
+
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  # この行を編集する
+  resources :posts, only: %i(index new create show destroy) do
+    resources :photos, only: %i(create)
+  end
 end
