3337a7dec746cbf1b0291a070f685984ad2efc9d
shun-tateyama <b1522783@planet.kanazawa-it.ac.jp>
2020-09-16 07:35:23 +0000

    フォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes
   has_many :comments
-  
+  acts_as_followable
+  acts_as_follower
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
