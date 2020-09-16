86dd08e1ee18e670a3aaefeb27e3f6ad595c0c54
shun-tateyama <b1522783@planet.kanazawa-it.ac.jp>
2020-09-16 10:43:36 +0000

    プロフィール追加（ウェブサイト、自己紹介）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:username])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name,:username])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name,:username,:website,
+                                        :self_introduction,:tel,:sex])
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_070632) do
+ActiveRecord::Schema.define(version: 2020_09_16_100938) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment", null: false
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2020_09_16_070632) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "follows", force: :cascade do |t|
@@ -82,6 +87,10 @@ ActiveRecord::Schema.define(version: 2020_09_16_070632) do
     t.string "name", default: "", null: false
     t.string "profile_photo"
     t.string "username", default: "", null: false
+    t.string "website"
+    t.text "self_introduction"
+    t.string "tel"
+    t.string "sex"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
